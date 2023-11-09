09450a541a24ad7915f9cd202c6b6304eda560d0
SimoneVilardo <simovila95@gmail.com>
2023-11-09 18:18:25 +0100

    esercizio completato + bonus

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -3,7 +3,16 @@ package org.lessons.java.array;
 public class CibiPreferiti {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String[] cibiPreferiti = {"Carbonara", "Lasagne", "Pizza", "Cheesecake", "Cookies"};
+		
+		System.out.println(cibiPreferiti.length);
+		
+		System.out.println(cibiPreferiti[0]);
+		
+		System.out.println(cibiPreferiti[cibiPreferiti.length - 1]);
+		
+		System.out.println(cibiPreferiti[cibiPreferiti.length / 2]);
 
 	}
 
